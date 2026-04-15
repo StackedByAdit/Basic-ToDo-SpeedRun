4e0f794515f03e50b5e2d5ec46699dcbe0ca6392
Aditya <adityaguptavns1@gmail.com>
2026-04-16 03:52:31 +0530

    fixed bugs

--- backend/index.ts
+++ backend/index.ts
@@ -26,65 +26,69 @@ let todoIdCounter = 1;
 
 
 app.post("/signup", async (req: Request, res: Response) => {
-    const response = signupSchema.safeParse(req.body);
-    if (!response.success) {
-        return res.status(403).json({
-            success: false,
-            message: "credentials required"
-        })
-    }
-    
-    const data = response.data;
-
-    const existingUser = users.find((elem => elem.email == data.email));
-
-    if(existingUser){
-        return res.status(403).json({
-            message : "User already exists"
-        })
-    }
-    users.push({
-        id: userIdCounter,
-        username: data.username,
-        email: data.email,
-        password: data.password
-
-    });
-
-    userIdCounter++;
-
-    res.status(200).json({
-        success: true,
-        message: "User created",
-        email: data.email,
-        username: data.username
+  const response = signupSchema.safeParse(req.body);
+  if (!response.success) {
+    return res.status(403).json({
+      success: false,
+      message: "credentials required"
     })
+  }
+
+  const data = response.data;
+
+  const existingUser = users.find((elem => elem.email == data.email));
+
+  if (existingUser) {
+    return res.status(403).json({
+      message: "User already exists"
+    })
+  }
+  users.push({
+    id: userIdCounter,
+    username: data.username,
+    email: data.email,
+    password: data.password
+
+  });
+
+  userIdCounter++;
+
+  res.status(200).json({
+    success: true,
+    message: "User created",
+    email: data.email,
+    username: data.username
+  })
 })
 
-app.post("/signin", (req : Request, res : Response) => {
+app.post("/signin", (req: Request, res: Response) => {
 
-    const response = signinSchema.safeParse(req.body);
+  const response = signinSchema.safeParse(req.body);
 
-    if (!response.success) {
-        return res.status(403).json({
-            success: false,
-            message: "Something went wrong"
-        })
-    }
-
-    const data = response.data;
-
-    const token = jwt.sign({ userId: data.email }, SECRET)
-
-    res.status(200).json({
-        success: true,
-        token: token
+  if (!response.success) {
+    return res.status(403).json({
+      success: false,
+      message: "Something went wrong"
     })
+  }
+
+  const data = response.data;
+
+  const token = jwt.sign({ userId: data.email }, SECRET)
+
+  res.status(200).json({
+    success: true,
+    token: token
+  })
 
 })
 
-app.post("/todo", authMiddleware, (req: Request , res: Response) => {
+app.post("/todo", authMiddleware, (req: Request, res: Response) => {
   const response = todoSchema.safeParse(req.body);
+
+  console.log("BODY:", req.body);
+
+  console.log(response);
 
   if (!response.success) {
     return res.status(403).json({
@@ -115,6 +119,26 @@ app.post("/todo", authMiddleware, (req: Request , res: Response) => {
   });
 });
 
+app.get("/todos", authMiddleware, (req: Request, res: Response) => {
+  const userId = req.user?.userId;
+
+  if (!userId) {
+    return res.status(403).json({
+      success: false,
+      message: "Unauthorized",
+    });
+  }
+
+  const response = todos.filter((user => user.userId == userId));
+
+  console.log(response);
+
+  res.status(200).json({
+    success: true,
+    todos: response,
+  });
+})
+
 app.listen(PORT, () => {
-    console.log(`Backend running on http://localhost:${PORT}`);
+  console.log(`Backend running on http://localhost:${PORT}`);
 });
