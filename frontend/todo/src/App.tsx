df20a49fa101e50ec418e0aa00758c6fdc6dfab8
Aditya <adityaguptavns1@gmail.com>
2026-04-16 20:16:46 +0530

    adjusted changes

--- frontend/todo/src/App.tsx
+++ frontend/todo/src/App.tsx
@@ -3,6 +3,7 @@ import Signup from './components/pages/Signup'
 import Todos from './components/pages/Todos'
 
 const App = () => {
+
   return (
     <div className='bg-gray-900 h-screen py-10 flex flex-col gap-10 justify-center items-center'>
       <Signup />
