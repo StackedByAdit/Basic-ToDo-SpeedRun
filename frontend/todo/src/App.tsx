5799ec1eb3a66f29f646dcd9770259cdb6500521
Aditya <adityaguptavns1@gmail.com>
2026-04-16 23:00:56 +0530

    fixed bugs and made changes

--- frontend/todo/src/App.tsx
+++ frontend/todo/src/App.tsx
@@ -1,14 +1,17 @@
 import Login from './components/pages/Login'
 import Signup from './components/pages/Signup'
 import Todos from './components/pages/Todos'
-
+import { Navigate, Route, Routes } from "react-router-dom"
 const App = () => {
 
   return (
     <div className='bg-gray-900 h-screen py-10 flex flex-col gap-10 justify-center items-center'>
-      <Signup />
-      <Login />
-      <Todos />
+      <Routes>
+        <Route path='/' element={<Navigate to='/signup' replace />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/todos" element={<Todos />} />
+      </Routes>
     </div>
   )
 }
