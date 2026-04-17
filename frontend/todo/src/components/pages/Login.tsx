27b17ba1dbdba508798b08d481246f37ba86ee74
Aditya <adityaguptavns1@gmail.com>
2026-04-17 15:05:17 +0530

    added tailwind css to login

--- frontend/todo/src/components/pages/Login.tsx
+++ frontend/todo/src/components/pages/Login.tsx
@@ -46,15 +46,18 @@ export const Login = () => {
     }
 
     return (
-        <div className="flex flex-col gap-10 text-amber-50" >
+        <div className="flex flex-col items-center justify-center p-5 gap-5 text-amber-50
+         bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl
+         bg-gradient-to-r from-gray-900 to-white-900
+         shadow-[0_0_20px_rgba(111,111,111,0.4)]" >
             <div>
-                <input type="email" placeholder="enter email" onChange={(e) => { setEmail(e.target.value) }} value={email} />
+                <input className="border-1 hover:scale-110 transition duration-200 active:scale-95 hover:bg-gray-800 rounded-2xl p-5" type="email" placeholder="enter email" onChange={(e) => { setEmail(e.target.value) }} value={email} />
             </div>
             <div>
-                <input type="password" placeholder="enter password" onChange={(e) => { setPassword(e.target.value) }} value={password} />
+                <input className="border-1 hover:scale-110 transition duration-200 active:scale-95 hover:bg-gray-800 rounded-2xl p-5" type="password" placeholder="enter password" onChange={(e) => { setPassword(e.target.value) }} value={password} />
             </div>
             <div>
-                <button onClick={handleSubmit}> Login</button>
+                <button className="border-1 hover:scale-110 transition duration-200 active:scale-95 hover:bg-gray-800 rounded-2xl p-5" onClick={handleSubmit}> Login</button>
             </div>
         </div>
     )
