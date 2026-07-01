9d7cc01331022f2f3c50382eb0eebaf170617d3e
Aditya <adityaguptavns1@gmail.com>
2026-07-01 20:40:19 +0530

    add check

--- frontend/todo/src/App.tsx
+++ frontend/todo/src/App.tsx
@@ -5,7 +5,7 @@ import { Navigate, Route, Routes } from "react-router-dom"
 const App = () => {
 
   return (
-    <div className='bg-gray-900 h-screen py-10 flex flex-col gap-10 justify-center items-center'>
+    <div className='bg-zinc-950 h-screen py-10 flex flex-col gap-10 justify-center items-center'>
       <Routes>
         <Route path='/' element={<Navigate to='/signup' replace />} />
         <Route path="/signup" element={<Signup />} />
